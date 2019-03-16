4d9768f7991974e0d69f22206f55cc0e82cc2a0c
Jody Frankowski <jody.frankowski@gmail.com>
2019-03-16 19:09:20 +0100

    fix: do the macos installation step before using the stow command

--- user-only.sh
+++ user-only.sh
@@ -11,38 +11,6 @@ for dir in ~/.usr/bin/ ~/.usr/opt ~/.usr/var/log ~/code/ ~/code/tmp ; do
     [[ -d "${dir}" ]] || mkdir -p "${dir}"
 done
 
-# mpd
-if [[ ! -d ~/.mpd ]] ; then
-    mkdir -p ~/.mpd/playlists
-    touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
-fi
-
-# ssh
-[[ -d ~/.ssh ]] || mkdir ~/.ssh && chmod 700 ~/.ssh
-[[ -d ~/.ssh/tmp ]] || mkdir ~/.ssh/tmp && chmod 700 ~/.ssh/tmp
-
-# tpm (tmux-plugin-manager)
-[[ -d ~/.tmux/plugins/tpm ]] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# install plugins. buggy, better use prefix + I
-~/.tmux/plugins/tpm/scripts/install_plugins.sh > /dev/null
-
-# stow the dotfiles
-for dir in base docker emacs gnupg js mpd mpv ssh tmux valgrind X ; do
-    stow --no-folding ${dir}
-done
-# hack for the freaking symlink removal
-chmod 500 ~/.config/gtk-2.0/
-
-ls -d *-"$(hostname)" &>/dev/null && stow --no-folding *-"$(hostname)"
-
-# Linux Specific
-if [[ "$OSTYPE" == "linux-gnu" ]] ; then
-    # Reload systemd because of systemd units
-    systemctl --user daemon-reload
-
-    # change file-chooser startup location in gtk 3 https://wiki.archlinux.org/index.php/GTK%2B#File-Chooser_Startup-Location
-    gsettings set org.gtk.Settings.FileChooser startup-mode cwd
-fi
 # macOS Specific
 if [[ "$OSTYPE" == "darwin"* ]]; then
     [[ -d ~/.brew ]] || git clone --depth=1 https://github.com/Homebrew/brew ~/.brew
@@ -76,6 +44,39 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     for symlink in date dircolors ls rm ; do
         [[ -L ~/.usr/bin/$symlink ]] || ln -s ~/.brew/bin/g$symlink ~/.usr/bin/$symlink
     done
+fi
+
+# mpd
+if [[ ! -d ~/.mpd ]] ; then
+    mkdir -p ~/.mpd/playlists
+    touch ~/.mpd/{mpd.db,mpd.log,mpd.pid,mpdstate}
+fi
+
+# ssh
+[[ -d ~/.ssh ]] || mkdir ~/.ssh && chmod 700 ~/.ssh
+[[ -d ~/.ssh/tmp ]] || mkdir ~/.ssh/tmp && chmod 700 ~/.ssh/tmp
+
+# tpm (tmux-plugin-manager)
+[[ -d ~/.tmux/plugins/tpm ]] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# install plugins. buggy, better use prefix + I
+~/.tmux/plugins/tpm/scripts/install_plugins.sh > /dev/null
+
+# stow the dotfiles
+for dir in base docker emacs gnupg js mpd mpv ssh tmux valgrind X ; do
+    stow --no-folding ${dir}
+done
+# hack for the freaking symlink removal
+chmod 500 ~/.config/gtk-2.0/
+
+ls -d *-"$(hostname)" &>/dev/null && stow --no-folding *-"$(hostname)"
+
+# Linux Specific
+if [[ "$OSTYPE" == "linux-gnu" ]] ; then
+    # Reload systemd because of systemd units
+    systemctl --user daemon-reload
+
+    # change file-chooser startup location in gtk 3 https://wiki.archlinux.org/index.php/GTK%2B#File-Chooser_Startup-Location
+    gsettings set org.gtk.Settings.FileChooser startup-mode cwd
 fi
 
 # ssh config

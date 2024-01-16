7d86982274c4124af6176161c84204a606132c25
Jody Frankowski <jody.frankowski@gmail.com>
2024-01-16 11:16:35 +0100

    zsh: Change the date format of bak() files to allow an easier double click selection

--- zsh/.zshrc.d/functions.zsh
+++ zsh/.zshrc.d/functions.zsh
@@ -5,8 +5,7 @@ bak () {
         # Clean up the path in case it contains a trailing /, eg. "dir/"
         arg="$(dirname $arg)/$(basename $arg)"
 
-        newname="$arg-$(date -u +%F-%H-%M-%S)";
-        cp -a "$arg" "$newname";
+        cp -a "${arg}" "${arg}-$(date -u +%F%H%M%S).bak"
     done
 }
 

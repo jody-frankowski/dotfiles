f6546847a257b75b3be1ca4f0a21fb93bb66e570
Jody Frankowski <jody.frankowski@gmail.com>
2025-12-24 17:23:16 +0100

    feat/nvim Set the outer terminal title to the current file name

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,3 +4,17 @@ vim.opt.number = true     -- Enable line numbers
 vim.opt.background = "light"
 vim.opt.termguicolors = true
 vim.cmd.colorscheme("base16-standardized-light")
+
+-- Set outer terminal title
+vim.opt.title = true
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*" },
+  callback = function()
+    local title = "v"
+    local filename = vim.fn.expand("%:t") -- :t takes the tail
+    if filename ~= "" then
+      title = title .. " " .. filename
+    end
+    vim.opt.titlestring = title
+  end,
+})

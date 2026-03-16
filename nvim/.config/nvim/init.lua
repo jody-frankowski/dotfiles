6ead948785039f4abb82d552f303a7f57cfe6e3b
Jody Frankowski <jody.frankowski@gmail.com>
2026-03-16 22:04:53 +0100

    feat:nvim Add Emacs/Readline and <leader>-qq keybindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,6 +22,18 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 -- Keybindings
 -- Leader
 vim.g.mapleader = " "
+-- Misc
+vim.keymap.set("n", "<leader>qq", ":qa<CR>")
+-- Command-Line Mode Emacs Movements Keybindings
+vim.keymap.set("c", "<C-a>", "<Home>")
+vim.keymap.set("c", "<C-e>", "<End>")
+vim.keymap.set("c", "<C-p>", "<Up>")
+vim.keymap.set("c", "<C-n>", "<Down>")
+vim.keymap.set("c", "<C-b>", "<Left>")
+vim.keymap.set("c", "<C-f>", "<Right>")
+vim.keymap.set("c", "<M-b>", "<S-Left>")
+vim.keymap.set("c", "<M-f>", "<S-Right>")
+vim.keymap.set("c", "<M-BS>", "<C-w>")
 -- Copy, Comment, Paste
 vim.keymap.set("n", "<leader>cyy", "yygccp", { remap = true, desc = "Copy line, comment, paste below" })
 vim.keymap.set("x", "<leader>cy", "ygvgc'>p", { remap = true, desc = "Copy selection, comment, paste below" })
